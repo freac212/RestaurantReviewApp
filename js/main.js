6f054cfbd18b6cc1bedf8abfc7a3d1628489f9aa
DonnyG <donavingrouette@live.com>
2018-12-20 09:34:14 -0600

    Updated site to work locally
    Changed port in dbhelper to 5500 (VSCodes port)
    Added API key to main and restaurant info

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiZnJlYWMyMTIiLCJhIjoiY2pwd3I4ZGYzMGlodzQycWxkY3hyY2w4OSJ9.Cu3JYIHq_69eW9pebGcs9g',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
